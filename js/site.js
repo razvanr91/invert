7520df59bba047aaf14282e96909e54a1f9543f1
razvanr91 <razvanroman1991@gmail.com>
2021-07-13 21:30:17 +0100

    Implemented functionality

--- js/site.js
+++ js/site.js
@@ -1,4 +1,35 @@
-//Call Hello World
-function HelloWorld() {
-    alert("Hello World");
+// get the string from the input
+function getValue() {
+
+    document.getElementById("alert").classList.add("invisible");
+
+    let userInput = document.getElementById("userString").value;
+    let invertString = reverseString(userInput);
+
+    displayString(invertString);
 }
+
+
+// reverse the string
+function reverseString(stringToReverse) {
+    let reversedString = [];
+
+    for (let i = stringToReverse.length - 1; i >= 0; i--) {
+        reversedString += stringToReverse[i];
+    }
+
+    return reversedString.toString();
+}
+
+
+// display the reverse string
+function displayString(invertString) {
+
+    // write to the page
+    document.getElementById("message").innerHTML = `${invertString}`;
+
+    // show the alert box
+    document.getElementById("alert").classList.remove("invisible");
+}
+
+document.getElementById("btnSubmit").addEventListener("click", getValue);
